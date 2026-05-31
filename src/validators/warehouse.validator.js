a9c7048ddc8488042319009d9fdc375064e2718d
askribilel123 <askribilel09@gmail.com>
2026-05-31 15:35:25 +0100

    add min_stock column to warehouse table

--- src/validators/warehouse.validator.js
+++ src/validators/warehouse.validator.js
@@ -4,6 +4,7 @@ const createWarehouseSchema = z.object({
   name: z.string().min(2).max(50),
   address: z.string().min(2).max(250),
   type: z.enum(["physique", "logic"]),
+  min_stock: z.number().min(0).optional(),
 });
 
 const updateWarehouseSchema = createWarehouseSchema.partial();
